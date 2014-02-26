cfa9d874fb30af7d9095a90d6121c918117dafbc
Aldrin Jerome Almacin <aldrin.almacin@babyrobot.com>
2014-02-26 15:04:25 -0500

    Changed color

--- classes/Assault.js
+++ classes/Assault.js
@@ -2,7 +2,7 @@ var Assault = function(playerNum, xLine, yLine) {
   this.initialize(playerNum, xLine, yLine);
 }
 Assault.PLAYER_1_COLOR = "139, 69, 19";
-Assault.PLAYER_2_COLOR = "30, 30, 30";
+Assault.PLAYER_2_COLOR = "85, 107, 47";
 Assault.RADIUS = 40;
 Assault.SIDES = 10;
 Assault.POINT_SIZE = .2;
