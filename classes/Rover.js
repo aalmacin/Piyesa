02d95404509f8b9d3f8509f790c09746390cff22
Aldrin Jerome Almacin <aldrin.almacin@babyrobot.com>
2014-02-27 11:38:59 -0500

    Movement for rover added

--- classes/Rover.js
+++ classes/Rover.js
@@ -17,3 +17,24 @@ Rover.prototype.initialize = function(playerNum, xLine, yLine) {
   var objRotation = 70;
   this.Piece_initialize(color, xLine, yLine, Rover.RADIUS, Rover.SIDES, Rover.POINT_SIZE, objRotation);
 }
+
+Rover.prototype.handleClick = function(e) {
+  var tile = e.target.tile;
+  var board = tile.parent;
+  board.clickedPiece = tile.piece;
+  for(var i=0; i < board.tiles.length; i++){
+    var currTile = board.tiles[i];
+    if (currTile.highlighted) {
+      currTile.unhighlightTile();
+    }
+    var nw = (tile.xLine - 1 == currTile.xLine && tile.yLine - 1 == currTile.yLine);
+    var sw = (tile.xLine - 1 == currTile.xLine && tile.yLine + 1 == currTile.yLine);
+    var ne = (tile.xLine + 1 == currTile.xLine && tile.yLine - 1 == currTile.yLine);
+    var se = (tile.xLine + 1 == currTile.xLine && tile.yLine + 1 == currTile.yLine);
+    if(nw || ne || sw || se) {
+      if(currTile.piece == null) {
+        currTile.highlightTile();
+      }
+    }
+  }
+}
